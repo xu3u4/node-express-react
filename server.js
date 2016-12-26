b684aabd5d3b2fbeed57f167b4079256270845fc
Jocelyn Li <u4xu3@yahoo.com.tw>
2016-12-26 15:53:35 +0800

    Add HtmlWebpackPlugin to bundle scripts automatically

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 //for starting with express
 
 var express = require('express'),
-	app = express(),
- 	port = 8000,
+    path = require('path'),
+    app = express(),
+    port = 8000,
     router = express.Router();
+
+app.use(express.static(path.resolve(__dirname, 'dist')));
 
 router.get('/', function(req, res) {
     res.sendfile('./index.html');
 });
 
-router.get('/dist/bundle.js', function(req, res) {
-    res.sendfile('./dist/bundle.js');
-});
+
 //set up the router
-app.use('/', router);
+
 app.listen(port);
 console.log('server started on ' + port);
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,25 @@
 var path = require('path');
 var webpack = require("webpack");
 
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+    template: __dirname+'/index.html',
+    filename: 'index.html',
+    inject: 'body',
+});
+
+console.log(__dirname+'/dist/index.html');
+
 module.exports = {
     entry: [
-            'webpack/hot/dev-server', //for webpack-dev-server live reload
-            __dirname + "/app/js/app.jsx" ],
+        'webpack/hot/dev-server', //for webpack-dev-server live reload
+        __dirname + "/app/js/app.jsx"
+    ],
 
     output: {
         filename: "bundle.js",
         path: __dirname + "/dist",
-        publicPath: "/dist/"
+        publicPath: "/" //where index.html
     },
     module: {
          loaders: [
@@ -28,16 +38,32 @@ module.exports = {
     },
      stats: {
         colors: true
-    }
+    },
+    devServer: {
+        inline: true,
+        hot: true,
+        contentBase: './'
+    },
+    plugins: [HTMLWebpackPluginConfig,
+        new webpack.DefinePlugin({
+            "process.env": {
+                NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+            }
+        }),
+        new webpack.HotModuleReplacementPlugin()
+    ]
 }
+console.log(`NODE_ENV ${process.env.NODE_ENV}`);
 
-//webpack-dev-server, in package.json ln:9
-// webpack-dev-server 會在 localhost:8080 建立起專案的 server
+//package.json
 // --devtool eval 會顯示出發生錯誤的行數與檔案名稱
 // --progress 會顯示出打包的過程
 // --colors 會幫 webpack 顯示的訊息加入顏色
 // --content-based build 指向專案最終輸出的資料夾
-
+/*
+export NODE_ENV=development --for linux&osx
+set NODE_ENV=development --for windows
+*/
 /* for hot reload
     plugins: [
         new webpack.HotModuleReplacementPlugin()
@@ -46,5 +72,4 @@ module.exports = {
         hot: true,
         contentBase: './'
     }
-
 */
