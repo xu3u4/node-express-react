7561304532a743f0d9d4be104898fdac1088c4eb
Jocelyn Li <u4xu3@yahoo.com.tw>
2016-12-29 11:26:59 +0800

    Separte <tr> and <td> to different components

---
+++ app/js/cell.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class Cell extends React.Component{
+	render() {
+	    return (
+	    	<td>{this.props.value}</td>
+	    );
+  	}
+}
--- app/js/rows.jsx
+++ app/js/rows.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
-//console.log(1123);
+import Cell from './cell.jsx';
+
 export default class GenerateRow extends React.Component{
 	render() {
-		const geRow = this.props.columns.map( (col) =>
-			this.props.rows.map(
-				(info) => <td>{info[col.key]}</td>
-			)
-		)
+		const geRow = this.props.rows.map((row, i) =>
+			<tr key = {i} >
+				{
+					this.props.columns.map((col) =>
+						<Cell key = {col.key} value = {row[col.key]}/>
+					)
+				}
+			</tr>
+		);
+
 	    return (
-	    	<tr>{geRow}</tr>
+	    	<tbody>{geRow}</tbody>
 	    );
   	}
 }
