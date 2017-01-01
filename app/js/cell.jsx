ae62d254ccb7e5fb1f9f22bf50af6879b371fa46
Jocelyn Li <u4xu3@yahoo.com.tw>
2017-01-01 18:17:58 +0800

    Convert indentation to spaces

--- app/js/cell.jsx
+++ app/js/cell.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
 export default class Cell extends React.Component {
-	render() {
-		return (
-			<td>{this.props.value}</td>
-		);
-	}
+    render() {
+        return (
+            <td>{this.props.value}</td>
+        );
+    }
 }
 Cell.propTypes = {
-	value: React.PropTypes.string.isRequired
+    value: React.PropTypes.string.isRequired
 };
--- app/js/greeting.jsx
+++ app/js/greeting.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 export default class GreetingCom extends React.Component{
-	render() {
-	    return (
-	      <div className="greeting">
-	        Hello, big big {this.props.name}!
-	      </div>
-	    );
-  	}
+    render() {
+        return (
+            <div className="greeting">
+                Hello, big big {this.props.name}!
+            </div>
+        );
+    }
 };
