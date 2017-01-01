92085bd51c169fb337f6b7cfe0dddcb639e8a984
Jocelyn <u4xu3@yahoo.com.tw>
2017-01-01 22:08:29 +0800

    Run ESLint before testing and modify ESLint rules

--- test/client/greeting_test.js
+++ test/client/greeting_test.js
@@ -1,28 +1,27 @@
-require('../utils/dom-mock')('<html><body></body></html>');
-
-import jsdom from 'mocha-jsdom';
-import assert from 'assert';
+// import assert from 'assert';
 import React from 'react';
 import expect from 'expect.js';
 import TestUtils from 'react-addons-test-utils';
 import GreetingCom from '../../app/js/greeting';
 
+require('../utils/dom-mock')('<html><body></body></html>');
+
 //test message from Greeting component
 describe('Testing my div', () => {
-    jsdom({skipWindowCheck: true}); // skips checking of window at startup. When false, mocha-jsdom will throw an error if window already exists. Defaults to false
     var component = TestUtils.renderIntoDocument(
-        <GreetingCom  name = "Jocelyn"/>
+        <GreetingCom name = "Jocelyn"/>
     );
 
     it('render a div', () => {
         var div = TestUtils.findRenderedDOMComponentWithTag(component, 'div');
+
         // assert.equal(div.textContent, 'Hello, big big Jocelyn!');//using assert
         expect(div.textContent).to.equal('Hello, big big Jocelyn!');
     });
 
     it('render with className = greeting', () => {
         var classname = TestUtils.findRenderedDOMComponentWithClass(component, 'greeting');
+
         expect(classname.className).to.equal('greeting');
     });
 });
-
