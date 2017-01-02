d5b399b6e06b27ce22b1314eea6a830688f3bef0
Jocelyn <u4xu3@yahoo.com.tw>
2017-01-02 15:42:49 +0800

    Test table components

--- app/js/header.jsx
+++ app/js/header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default class GenerateHeader extends React.Component {
 
 	render() {
-		const geHead = this.props.columns.map((col) => <th key = {col.key} >{col.key} </th>);
+		const geHead = this.props.columns.map((col) => <th key = {col.key} >{col.key}</th>);
 
 		return (
 			<thead><tr>{geHead}</tr></thead>
