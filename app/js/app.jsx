1f1b92e75b0885b276ed7b6b8711402b0a457928
Jocelyn Li <u4xu3@yahoo.com.tw>
2017-03-12 14:51:59 +0800

    Remove unused redux-thunk

--- app/js/app.jsx
+++ app/js/app.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
 import reducers from './reducers/index.jsx';
 import TableFrame from './containers/table_frame.jsx';
 
 const App = () => <TableFrame />;
 
 ReactDOM.render(
-  <Provider store={createStore(reducers, applyMiddleware(thunk))}>
+  <Provider store={createStore(reducers)}>
     <App />
   </Provider>,
   document.getElementById('app')
