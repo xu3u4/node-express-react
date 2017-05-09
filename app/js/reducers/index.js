4344537041b8ba31441a3c844ab728d997159c72
Jocelyn Li <u4xu3@yahoo.com.tw>
2017-05-09 23:17:05 +0800

    Merge reducers

--- app/js/reducers/index.js
+++ app/js/reducers/index.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux';
 import IssuesReducer from './reducer_issues.js';
 import HeadsReducer from './reducer_heads.js';
-import ActiveReducer from './reducer_active.js';
 
 const rootReducer = combineReducers({
   IssuesReducer,
-  HeadsReducer,
-  ActiveReducer
+  HeadsReducer
 });
 
 export default rootReducer;
--- app/js/reducers/reducer_issues.js
+++ app/js/reducers/reducer_issues.js
@@ -1,25 +1,53 @@
-export default function (state = [
+export default function (state = {
+  issues: [
     { seq: '1', Status: 'Open', Category: 'cat1', Title: 'title1', Owner: 'Allen', Priority: '1' },
     { seq: '2', Status: 'Open', Category: 'cat1', Title: 'title1', Owner: 'Allen', Priority: '2' },
     { seq: '3', Status: 'Open', Category: 'cat1', Title: 'title1', Owner: 'Allen', Priority: '3' },
     { seq: '4', Status: 'Open', Category: 'cat1', Title: 'title1', Owner: 'Allen', Priority: '4' }
-], action) {
+  ],
+  selectedIssue: {},
+  newIssue: {},
+  isShowWarning: false
+}, action) {
   switch (action.type) {
     case 'ISSUE_DELETED':
-      return [
-        ...state.slice(0, action.payload),
-        ...state.slice(action.payload + 1)
-      ];
-    case 'UPDATE_ISSUES':
-      action.payload.seq = state.length ?
-                          (action.payload.seq ||
-                          (1 + Number(state[state.length - 1].seq))
-                          .toString())
-                          : '1';
-      return [
+      return {
         ...state,
+        issues: [
+          ...state.issues.slice(0, action.payload),
+          ...state.issues.slice(action.payload + 1)
+        ]
+      };
+    case 'UPDATE_ISSUES': {
+      action.payload.seq =
+        state.issues.length ?
+          (action.payload.seq || (1 + Number(state.issues[state.issues.length - 1].seq))
+          .toString())
+          : '1';
+      const orderedArray = [
+        ...state.issues,
         action.payload
       ];
+      orderedArray.sort((prev, next) => prev.seq - next.seq);
+      return {
+        isShowWarning: false,
+        newIssue: action.payload,
+        selectedIssue: {},
+        issues: orderedArray
+      };
+    }
+    case 'SHOW_WARNING':
+      return {
+        ...state,
+        isShowWarning: action.payload
+      };
+    case 'ACTIVE_ISSUE':
+      return {
+        ...state,
+        newIssue: {},
+        selectedIssue: action.payload,
+        isShowWarning: false
+      };
     default:
       return state;
   }
