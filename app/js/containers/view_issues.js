f3d5eeabfe1c71bf8f2b3ecef4ecebf10607a8a2
Jocelyn Li <u4xu3@yahoo.com.tw>
2017-05-16 21:06:43 +0800

    Change import css

--- app/js/containers/view_issues.js
+++ app/js/containers/view_issues.js
@@ -2,7 +2,6 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { editIssue, deleteIssue } from '../actions/action_index';
 
-import '../../css/table.scss';
 import ViewIssues from '../components/view_issues';
 
 const mapStateToProps = (state) => ({
