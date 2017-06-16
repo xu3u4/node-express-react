41ece89d07071ae75e6a69d09658207724a8d612
Jocelyn Li <u4xu3@yahoo.com.tw>
2017-06-16 23:30:14 +0800

    Use redux'thunk as middleware

--- app/js/app.js
+++ app/js/app.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import ReduxThunk from 'redux-thunk';
 
 import reducers from './reducers';
 import TableFrame from './components/table_frame';
 
+const stores = createStore(reducers, applyMiddleware(ReduxThunk));
 ReactDOM.render(
-  <Provider store={createStore(reducers)}>
+  <Provider store={stores}>
     <TableFrame />
   </Provider>,
   document.getElementById('app')
